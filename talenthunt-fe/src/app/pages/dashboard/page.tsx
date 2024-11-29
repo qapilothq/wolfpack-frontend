887bbdfe2f206214fdcbf91d921f321b82c0f790
anil3004 <anilkumar.ka@digitral.com>
2024-11-29 14:25:04 +0530

    dashboard

--- talenthunt-fe/src/app/pages/dashboard/page.tsx
+++ talenthunt-fe/src/app/pages/dashboard/page.tsx
@@ -1,27 +1,50 @@
 import React from 'react'
-import { Check, ChevronsUpDown } from "lucide-react"
- 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { Combobox } from './dropdown'
+import { Separator } from '@/components/ui/separator'
 import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command"
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from '@/components/ui/button'
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
+
 
 
 const index = () => {
   return (
     <div>
-      Recruiter Summary
+      <Card className="w-[550px]">
+        <CardHeader>
+          <CardTitle>Select Job Description</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ResizablePanelGroup
+            direction="horizontal"
+            className="max-w-md rounded-lg md:min-w-[500px]"
+          >
+            <ResizablePanel defaultSize={50}>
+              {/* <div className="flex h-[100px] items-center justify-center p-6"> */}
+                <Combobox />
+              {/* </div> */}
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel defaultSize={50}>
+                <Button> Upload Resume </Button>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+
+        </CardFooter>
+      </Card>
     </div>
   )
 }
