05c801b514b4c98fa9b80a5539d9b6ecc47cd96f
Anil Kumar Kaki <anilkumar.ka@digitral.com>
2024-11-29 13:14:24 +0530

    shadcn components added

--- talenthunt-fe/src/app/pages/dashboard/page.tsx
+++ talenthunt-fe/src/app/pages/dashboard/page.tsx
@@ -1,4 +1,22 @@
 import React from 'react'
+import { Check, ChevronsUpDown } from "lucide-react"
+ 
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
 
 const index = () => {
   return (
