0b48395198027225b62631e890139b7820ad66bf
nihad.u <nihad.u@digitral.com>
2024-11-29 16:17:10 +0530

    create role page

--- talenthunt-fe/src/app/custom-components/Navbar.tsx
+++ talenthunt-fe/src/app/custom-components/Navbar.tsx
@@ -1,5 +1,8 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
 const links = [
   {
     'title': 'Create role',
@@ -9,9 +12,14 @@ const links = [
     'title': 'Dashboard',
     'link': '/dashboard'
   },
+  {
+    'title':'Design Assessment',
+    'link':'/assessment'
+  }
 ]
 
 const Navbar = () => {
+  const pathname = usePathname()
   return (
     <div className='min-h-10 flex items-center bg-[#5aabab]'>
       <div className='w-full flex justify-between'>
@@ -21,7 +29,7 @@ const Navbar = () => {
         <div className='flex gap-10 font-bold'>
           {
             links.map((link) => (
-              <Link href={`${link.link}`} key={link.link} className='hover:text-white'>
+              <Link href={`${link.link}`} key={link.link} className={`hover:text-[#5483b5] ${pathname === link.link? 'text-[#314d6a] font-bold' : 'text-blue-200' }`}>
                 {link.title}
               </Link>
             ))
--- talenthunt-fe/src/app/layout.tsx
+++ talenthunt-fe/src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar/>
-        <div className="flex items-center justify-center h-full">
+        <div className="flex justify-center h-[90vh]">
         {children}
         </div>
       </body>
